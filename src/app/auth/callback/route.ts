dd8ec0f3c1bd34b04d005287cdcc73205ff3f093
smdsqb <backupsaqib@proton.me>
2026-03-22 20:33:44 +0800

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,16 +1,12 @@
-import { createClient } from '@/lib/supabase-server'
 import { NextResponse } from 'next/server'
+import { supabase } from '@/lib/supabase'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
 
   if (code) {
-    const supabase = await createClient()
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) {
-      return NextResponse.redirect(origin)
-    }
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
   return NextResponse.redirect(origin)
