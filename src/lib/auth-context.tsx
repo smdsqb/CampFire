a3757821dc22d3aa267c8030ca63e28299d49aa8
smdsqb <backupsaqib@proton.me>
2026-03-22 14:06:38 +0800

    Update auth-context.tsx to change implicit

--- src/lib/auth-context.tsx
+++ src/lib/auth-context.tsx
@@ -58,7 +58,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}`,
+        redirectTo: `${window.location.origin}/auth/callback`,
+        skipBrowserRedirect: false,
       },
     })
   }
